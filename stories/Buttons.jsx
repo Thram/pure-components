9e585944d91785461c1a940621e47653be4c1cdc
Thram <thramposo@gmail.com>
2017-05-21 16:03:49 +1200

    fix(stories): Improve stories

--- stories/Buttons.jsx
+++ stories/Buttons.jsx
@@ -1,18 +1,50 @@
 import React from 'react';
-import { object, text } from '@storybook/addon-knobs';
-import { Button, ButtonGroup, Group, Cell } from '../src';
+import { uniqueId, capitalize } from 'lodash';
+import { object } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
+import { Button, ButtonGroup, Group } from '../src';
+
+const DEFAULTS = {
+  types: ['default', 'primary', 'secondary', 'success', 'error', 'warning'],
+  props: {
+    active: false,
+    disabled: false,
+    hidden: false,
+    hover: false,
+    selected: false,
+    className: '',
+    submit: false,
+  },
+};
 
 const Grid = () => (
-  <Group container fullHeight>
+  <Group container fullHeight style={{ padding: '.5rem 1rem' }}>
+    <h2>Button Types:</h2>
+    {DEFAULTS.types.map(type => (
+      <Button
+        key={uniqueId('button_')}
+        {...object('Buttons', DEFAULTS.props)}
+        type={type}
+        style={{ margin: '.5rem' }}
+        onClick={action(`Button ${type}`)}
+      >
+        {capitalize(type)}
+      </Button>
+    ))}
+
     <Group container>
-      <Button onClick={action('Test')}>Test</Button>
-    </Group>
-    <Group container>
-      <ButtonGroup>
-        <Button onClick={action('One')}>One</Button>
-        <Button onClick={action('Two')} type="primary">Two</Button>
-        <Button onClick={action('Three')}>Three</Button>
+      <h2>Button Group:</h2>
+      <ButtonGroup style={{ margin: '.5rem' }}>
+        {DEFAULTS.types.map(type => (
+          <Button
+            key={uniqueId('button_')}
+            {...object('Buttons', DEFAULTS.props)}
+            type={type}
+            onClick={action(`Button ${type}`)}
+          >
+            {capitalize(type)}
+          </Button>
+        ))}
       </ButtonGroup>
     </Group>
   </Group>
--- stories/Grid.jsx
+++ stories/Grid.jsx
@@ -1,45 +1,52 @@
 import React from 'react';
+import { times } from 'lodash';
 import { object, text } from '@storybook/addon-knobs';
 import { Group, Cell } from '../src';
 
+const COLORS = {
+  default: '#E6E6E6',
+  primary: 'rgb(0, 120, 231)',
+  secondary: 'rgb(66, 184, 221)',
+  success: 'rgb(66, 184, 221);',
+  error: 'rgb(202, 60, 60)',
+  warning: 'rgb(223, 117, 20)',
+};
+
 const DEFAULTS = {
+  fullHeight: false,
+  size: '1',
   mediaQueries: {
-    small: '1',
-    medium: '1/4',
-    large: '1/2',
-    xlarge: '1/3',
+    small: '1/2',
+    medium: '1/3',
+    large: '1/4',
+    extraLarge: '1/5',
   },
 };
+
 const cellStyle = {
-  height: '50px',
-  backgroundColor: '#A8CABA',
+  minHeight: '50px',
+  backgroundColor: COLORS.secondary,
   border: '1px solid #838689',
 };
 const Grid = () => (
-  <Group container fullHeight>
+  <Group container fullHeight style={{ padding: '.5rem 1rem' }}>
     <Group
       container
-      fullHeight
-      style={{ padding: '1rem', backgroundColor: 'blue' }}
+      style={{
+        padding: '1rem',
+        margin: '1rem 0',
+        backgroundColor: COLORS.primary,
+      }}
     >
-      <h1>Group Container</h1>
+      <h2>Group Container</h2>
     </Group>
-    <Group>
-      <Cell
-        size={text('Default Size Cell 1', '1')}
-        mediaQueries={object('Media Queries Cell 1', DEFAULTS.mediaQueries)}
-        style={cellStyle}
-      />
-      <Cell
-        size={text('Default Size Cell 2', '1')}
-        mediaQueries={object('Media Queries Cell 2', DEFAULTS.mediaQueries)}
-        style={cellStyle}
-      />
-      <Cell
-        size={text('Default Size Cell 3', '1')}
-        mediaQueries={object('Media Queries Cell 3', DEFAULTS.mediaQueries)}
-        style={cellStyle}
-      />
+    <h2>Grid System</h2>
+    <h3>Bases: 2,3,4,5,6,8,12,24</h3>
+    <h4>Separators: - / |</h4>
+    <Group fullHeight>
+      {times(5, () => (
+        <Cell {...object('Cells', DEFAULTS)} style={cellStyle} />
+      ))}
     </Group>
   </Group>
 );
